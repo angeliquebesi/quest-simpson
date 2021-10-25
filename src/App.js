ccdc6c27b7b78d947cc5b249ae219165d8b1a64d
angeliquebesi <besineaud85@gmail.com>
2021-10-25 12:31:18 +0200

    first perso

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import axios from 'axios';
 import './App.css';
 import QuoteCard from './components/QuoteCard';
 
+const first = {
+  quote: "By chilling my loins I increase the chances of impregnating my wife.",
+  character: "Apu Nahasapeemapetilon",
+  image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
+  characterDirection: "Left"
+}
 function App() {
-  const [simpsons, setSimpsons] = React.useState('');
+  const [simpsons, setSimpsons] = React.useState(first);
   const getSimpsons = ()=> {
     axios
       .get('https://simpsons-quotes-api.herokuapp.com/quotes')
